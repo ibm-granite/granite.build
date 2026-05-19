c17b22324f2ee3512522bb59fa13227905ef5a88
David Wood <dawood@us.ibm.com>
2026-05-19 13:07:17 -0400

    Move lsf buildtest to use buildtest.yaml

--- test/integration/ibm/buildrunner/lsf/test_buildrunner_1step_bluevela.py
+++ test/integration/ibm/buildrunner/lsf/test_buildrunner_1step_bluevela.py
@@ -1,36 +1,27 @@
-import os
-from typing import Self
+# Copyright LLM.build Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+"""YAML-driven equivalent of TestBuildRunner1StepBlueVela.
+
+The fixture's build.yaml and buildtest.yaml live in the directory returned by
+_get_yaml_spec_dir below.
+"""
+
+from pathlib import Path
 
 import pytest
 from lib.buildwatcher.buildtest import (
-    AbstractBuildRunnerTest,
-    BuildTestSpecification,
-    ClassTestedEnum,
-    ExpectedTarget,
+    AbstractYamlBuildRunnerTest,
+    get_test_data_dir_for,
 )
 from lib.constants import extended_testing_only
 
 pytestmark = pytest.mark.ibm
-
-_src_file_dir = os.path.abspath(os.path.dirname(__file__))
-_test_data_dir = _src_file_dir.replace("test", "test-data", 1)
-
-
-OneStepBlueVelaDownloadTestConfig = BuildTestSpecification(
-    build_yaml=os.path.join(_test_data_dir, "1step/bluevela/build.yaml"),
-    targets=["download_file"],
-    target_expections=[
-        # TODO: is this true for BlueVela?
-        # CPU env has 5 steps (not 4 like GPU) since it does not mount the AFM and does an lhpull step instead.
-        ExpectedTarget(
-            target_name="download_file",
-            step_count=5,
-            input_artifact_count=1,
-            output_artifact_count=1,
-            jobstats_count=3,
-        ),
-    ],
-)
 
 
 # @pytest.mark.skipif(
@@ -41,16 +32,7 @@ OneStepBlueVelaDownloadTestConfig = BuildTestSpecification(
 @extended_testing_only
 # @pytest.mark.skip
 @pytest.mark.xdist_group(name="buildtest_bv")
-class TestBuildRunner1StepBlueVela(AbstractBuildRunnerTest):
+class TestBuildRunner1StepBlueVela(AbstractYamlBuildRunnerTest):
 
-    def _get_test_specification(self: Self) -> BuildTestSpecification:
-        return OneStepBlueVelaDownloadTestConfig
-
-    def test_runner_cancellation(self):
-        """Test simple build cancelation on our 1step build.yaml.  We don't need to test this on every build we want to test, so just included it here"""
-        test_spec = self._get_test_specification()
-        self._run_build_test(
-            tested_class=ClassTestedEnum.TEST_BUILDRUNNER,
-            test_spec=test_spec,
-            test_cancel=True,
-        )
+    def _get_yaml_spec_dir(self) -> Path:
+        return get_test_data_dir_for(__file__) / "1step/bluevela"
