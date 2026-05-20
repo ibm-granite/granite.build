7d43e9f16cfbe23ff8d7cef3f8b25462d88baa20
David Wood <dawood@us.ibm.com>
2026-05-20 10:15:08 -0400

    Update Digit/sftfull/fmeval test to use buildtest.yaml

--- test/integration/ibm/buildrunner/k8s/test_builds.py
+++ test/integration/ibm/buildrunner/k8s/test_builds.py
@@ -1,58 +1,17 @@
-import os
-from typing import Self
+from pathlib import Path
 
 import pytest
 from lib.buildrunner.buildtest import (
-    AbstractBuildRunnerTest,
-    BuildTestSpecification,
-    ExpectedTarget,
+    AbstractYamlBuildRunnerTest,
+    get_test_data_dir_for,
 )
 from lib.constants import extended_testing_only
 
 pytestmark = pytest.mark.ibm
 
-_src_file_dir = os.path.abspath(os.path.dirname(__file__))
-_test_data_dir = _src_file_dir.replace("test", "test-data", 1)
-
 
 @extended_testing_only
-class AbstractLongBuildTest(AbstractBuildRunnerTest):
-    """Defined just so we can share the skip definition above."""
+class TestDiGiT_SFTFull_FMEval(AbstractYamlBuildRunnerTest):
 
-    pass
-
-
-DiGiT_SFTFull_FMEval = BuildTestSpecification(
-    build_yaml=os.path.join(_test_data_dir, "builds/DiGiT_SFTFull_FMEval/build.yaml"),
-    targets=None,
-    target_expections=[
-        ExpectedTarget(
-            target_name="syntheticdatageneration",
-            step_count=3,
-            input_artifact_count=1,
-            output_artifact_count=1,
-            jobstats_count=1,
-        ),
-        ExpectedTarget(
-            target_name="tunedmodel",
-            step_count=6,  # 2 inputs, 1 tuning, 2 outputs, one of which has 2 checkpoints = 6
-            input_artifact_count=2,
-            output_artifact_count=2,
-            jobstats_count=3,
-        ),
-        ExpectedTarget(
-            target_name="evalresults",
-            step_count=3,  # 1 input, 1 step, 1 output
-            input_artifact_count=1,
-            output_artifact_count=1,
-            jobstats_count=1,
-        ),
-    ],
-    timeout_minutes=120,
-)
-
-
-class TestDiGiT_SFTFull_FMEval(AbstractLongBuildTest):
-
-    def _get_test_specification(self: Self) -> BuildTestSpecification:
-        return DiGiT_SFTFull_FMEval
+    def _get_yaml_spec_dir(self) -> Path:
+        return get_test_data_dir_for(__file__) / "builds/DiGiT_SFTFull_FMEval"
