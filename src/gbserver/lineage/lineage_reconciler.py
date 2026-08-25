a243c5f5f1f74c9db126c420057305ba8c17a2cc
Valeria Becker <beckerv@us.ibm.com>
2026-08-25 12:53:14 -0300

    Log when a target is skipped for wandb lineage due to no output artifacts
    Signed-off-by: Valeria Becker <beckerv@us.ibm.com>

--- src/gbserver/lineage/lineage_reconciler.py
+++ src/gbserver/lineage/lineage_reconciler.py
@@ -473,6 +473,12 @@ def select_recordable_targets(
             if target.finished_at is None:
                 continue
             if not any(target.output_artifacts.values()):
+                logger.debug(
+                    "Skipping wandb lineage recording for target %s (build %s): "
+                    "no output artifacts.",
+                    target.uuid,
+                    build_id,
+                )
                 continue
             selected.append(target)
         if len(page) < _SCAN_PAGE_SIZE:
